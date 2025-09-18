180b3dfc398cd054f3fc3f06d94df933fb049520
Hyurim <kyo2301@naver.com>
2025-09-18 21:36:12 +0900

    refactor(thymeleaf): update annotations for Thymeleaf usage

--- src/main/java/com/hyuri/kanji_study/controller/KanjiPageController.java
+++ src/main/java/com/hyuri/kanji_study/controller/KanjiPageController.java
@@ -1,18 +1,20 @@
 // src/main/java/com/hyuri/kanji_study/controller/KanjiPageController.java
 package com.hyuri.kanji_study.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import com.hyuri.kanji_study.dto.KanjiDto;
 import com.hyuri.kanji_study.service.KanjiService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RequiredArgsConstructor
-@RestController
+@Controller
 @Slf4j
 @RequestMapping("kanji")
 public class KanjiPageController {
@@ -25,5 +27,12 @@ public class KanjiPageController {
         List<KanjiDto> list = kanjiService.getListAll();
         log.debug("result size = {}", list.size());
         return list; // JSON으로 응답
+    }
+    @GetMapping("/{id}")
+    public String detail(@PathVariable Long id, Model model) {
+        KanjiDto dto = kanjiService.readById(id);
+        model.addAttribute("kanji", dto);
+        log.debug("dto = {}",dto);
+        return "kanji-detail";
     }
 }

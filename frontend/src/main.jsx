ef5bfd4ffc7ad7af586c790b63d9086b8382a3a7
Hyurim <kyo2301@naver.com>
2025-09-25 01:43:19 +0900

    feat(frontend): add createRoot setup and BrowserRouter for routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={import.meta.env.BASE_URL || "/"}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
-)
+);

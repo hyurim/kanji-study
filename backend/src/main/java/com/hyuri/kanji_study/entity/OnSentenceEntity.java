f4986e96e21f105c3de0d5183ec935bf45620749
Hyurim <kyo2301@naver.com>
2025-09-28 01:16:57 +0900

    feat(entity): create Entity files for new tables

---
+++ backend/src/main/java/com/hyuri/kanji_study/entity/OnSentenceEntity.java
@@ -0,0 +1,39 @@
+package com.hyuri.kanji_study.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "on_sentence")
+public class OnSentenceEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "kanji_id", nullable = false)
+    private KanjiEntity kanji;
+
+    @Column(name = "on_jp_text")   // 음독 단어 문장
+    private String ounJpText;
+
+    @Column(name = "on_kr_text")    // 문장 뜻
+    private String ounKrText;
+}

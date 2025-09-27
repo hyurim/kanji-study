7241158f2f2a4278458c22bd53428ef0d5f4ac15
Hyurim <kyo2301@naver.com>
2025-09-28 01:14:22 +0900

    chore(config): update CorsConfig to allow local execution

--- backend/src/main/java/com/hyuri/kanji_study/security/CorsConfig.java
+++ backend/src/main/java/com/hyuri/kanji_study/security/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.hyuri.kanji_study.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,6 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
         r.addMapping("/api/**")
                 .allowedOrigins(
                         "http://localhost:4173",
+                        "http://localhost:5173",
                         "https://hyurim.github.io"          // 사용자 페이지
                 )
                 .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")

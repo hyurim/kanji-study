e103be80f8a4cf109e70d6e7a78813dc22d820c5
Hyurim <kyo2301@naver.com>
2025-09-30 03:12:46 +0900

    chore(deploy): redeploy application test

--- frontend/src/pages/Study.jsx
+++ frontend/src/pages/Study.jsx
@@ -145,7 +145,7 @@ const Study = () => {
           }}
         >
           <div style={{ fontSize: 48, lineHeight: 1.2 }}>{cur.glyph}</div>
-          <div style={{ fontSize: 18, marginTop: 8 }}>{cur.meaning} {cur.reading}</div>
+          <div style={{ fontSize: 18, marginTop: 8 }}>뜻 : {cur.meaning} 음 : {cur.reading}</div>
           <div style={{ marginTop: 8 }}>훈독: {cur.kunyomi || "-"}</div>
           <div>음독: {cur.onyomi || "-"}</div>
 

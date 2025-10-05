f224dbcab4328ec793548cfe4e2318295f762f32
Hyurim <kyo2301@naver.com>
2025-10-05 19:51:04 +0900

    refactor(security): remove duplicate PasswordEncoder field

--- backend/src/main/java/com/hyuri/kanji_study/controller/UserController.java
+++ backend/src/main/java/com/hyuri/kanji_study/controller/UserController.java
@@ -5,7 +5,6 @@ import com.hyuri.kanji_study.service.KanjiService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,12 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final KanjiService kanjiService;
-    private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/signup")
     public ResponseEntity<UserDto> signup(@RequestBody UserDto userDto) {
 
-        userDto.setPassword(passwordEncoder.encode(userDto.getPassword()));
 
         if (userDto.getRole() == null || userDto.getRole().isBlank()) {
             userDto.setRole("USER");

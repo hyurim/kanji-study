deee7ec47f6daff1649b495363388f11d6dcd71f
Hyurim <kyo2301@naver.com>
2025-10-15 00:41:20 +0900

    chore(deploy): update main for deployment testing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom"; // 서버에서 URL 처리
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./auth/AuthProvider";
 import GlobalStyle from "./styles/GlobalStyle";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL || "/"}>
+	<HashRouter>
+    {/* <BrowserRouter basename={import.meta.env.BASE_URL || "/"}> */}
 	  	<AuthProvider>
 		  	<GlobalStyle />
 			  <App />
 		  </AuthProvider>
-    </BrowserRouter>
+    {/* </BrowserRouter> */}
+	</HashRouter>
   </React.StrictMode>
 );
